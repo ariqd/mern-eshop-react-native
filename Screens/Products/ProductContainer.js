67cc7a363069f587fb277b8a9b7d4b1362b3740c
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-05 14:18:10 +0700

    folder setup

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { View, StyleSheet, ActivityIndicator, Text } from "react-native";
+
+const ProductContainer = () => {
+  return <View>
+      <Text>Product Container</Text>
+  </View>;
+};
+
+export default ProductContainer;
