39b0bd4f1eec5124ede7c0799e25757e0da563f4
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-02-09 11:06:53 +0700

    more product detail, init redux

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { LogBox } from "react-native";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
 // Navigators
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "./Navigators/Main";
 
 // Screens
-import ProductContainer from "./Screens/Products/ProductContainer";
+// import ProductContainer from "./Screens/Products/ProductContainer";
 import Header from "./Shared/Header";
 
 LogBox.ignoreAllLogs(true);
